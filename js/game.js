796e725df54ae50c53777f91ce8b22b3e8dbeea6
Heloise Ouvry <heloiseouvry@gmail.com>
2021-03-16 18:53:58 +0100

    Moving triangle with keyboard

--- js/game.js
+++ js/game.js
@@ -1,9 +1,17 @@
 let game = {
     gameContainer: document.getElementById("game-container"),
+    canvas: document.getElementById("tutorial"),
+    canvasWidth: document.getElementById("tutorial").width,
+    canvasHeight: document.getElementById("tutorial").height,
+    triangleWidth: document.getElementById("tutorial").width / 10,
+    triangleHeight: document.getElementById("tutorial").height / 10,
+    currentPosX: 0,
+    currentPosY: 0,
     init: function () {
         game.initGrid();
         game.initAvatar();
-
+        game.initEventListener();
+        game.displayTriangle();
     },
     initGrid: function (sizeGrid = 10, sizeCase = 30) {
         for (var i = 0; i < sizeGrid; i++) {
@@ -21,44 +29,75 @@ let game = {
     initAvatar: function () {
         // let firstCase = document.getElementById(`case-grid-0-0`);
     },
-
+    initEventListener: function () {
+        document.addEventListener("keydown", game.handleKeyDown);
+    },
+    cleanCanvas: function () {
+        var ctx = game.canvas.getContext("2d");
+        ctx.clearRect(0, 0, game.canvasWidth, game.canvasHeight);
+    },
+    displayTriangle: function (x = 0, y = 0, dir = "right") {
+        var ctx = game.canvas.getContext("2d");
+        var triangle = new Path2D();
+        ctx.fillStyle = 'rgb(200,0,0)';
+        ctx.beginPath();
+        switch (dir) {
+            case "right":
+                triangle.moveTo(x + 5, y + 5);
+                triangle.lineTo(x + 5, y + (game.triangleWidth-5));
+                triangle.lineTo(x + (game.triangleWidth-5), y + (game.triangleWidth/2));
+                break;
+            case "left":
+                triangle.moveTo(x + (game.triangleWidth-5), y + 5);
+                triangle.lineTo(x + (game.triangleWidth-5), y + (game.triangleWidth-5));
+                triangle.lineTo(x + 5, y + (game.triangleWidth/2));
+                break;
+            case "up":
+                triangle.moveTo(x + 5, y + (game.triangleHeight-5));
+                triangle.lineTo(x + (game.triangleHeight-5), y + (game.triangleHeight-5));
+                triangle.lineTo(x + (game.triangleHeight/2), y + 5);
+                break;
+            case "down":
+                triangle.moveTo(x + 5, y + 5);
+                triangle.lineTo(x + (game.triangleHeight-5), y + 5);
+                triangle.lineTo(x + (game.triangleHeight/2), y + (game.triangleHeight-5));
+                break;
+        }
+        ctx.fill(triangle);
+    },
+    moveTriangle: function (direction) {
+        game.cleanCanvas();
+        game.displayTriangle(this.currentPosX, this.currentPosY, direction);
+    },
+    handleKeyDown: function (event) {
+        switch (event.code) {
+            case "ArrowRight":
+                if (game.currentPosX < (game.canvasWidth - game.triangleWidth)) {
+                    game.currentPosX += game.triangleWidth;
+                }
+                game.moveTriangle("right");
+                break;
+            case "ArrowLeft":
+                if (game.currentPosX > 0) {
+                    game.currentPosX -= game.triangleWidth;
+                }
+                game.moveTriangle("left");
+                break;
+            case "ArrowUp":
+                if (game.currentPosY > 0) {
+                game.currentPosY -= game.triangleHeight;
+                }
+                game.moveTriangle("up");
+                break;
+            case "ArrowDown":
+                if (game.currentPosY < (game.canvasHeight - game.triangleHeight)) {
+                    game.currentPosY += game.triangleHeight;
+                    }
+                game.moveTriangle("down");
+                break;
+        }
+    },
 }
 
 game.init();
 
-var canvas = document.getElementById("tutorial");
-if (canvas.getContext) {
-    var ctx = canvas.getContext("2d");
-
-    ctx.fillStyle = 'rgb(200,0,0)';
-    ctx.fillRect(10, 10, 50, 50);
-
-    ctx.fillStyle = 'rgba(0,0,200,0.5)';
-    ctx.fillRect(30, 30, 50, 50);
-
-    ctx.beginPath();
-    ctx.moveTo(100, 50);
-    ctx.lineTo(150, 75);
-    ctx.lineTo(150, 25);
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.arc(150, 150, 50, 0, (Math.PI * 2));
-    ctx.moveTo(135, 130);
-    ctx.arc(130, 130, 5, 0, (Math.PI * 2));
-    ctx.moveTo(175, 130);
-    ctx.arc(170, 130, 5, 0, (Math.PI * 2));
-    ctx.moveTo(180, 150);
-    ctx.arc(150, 150, 30, 0, Math.PI);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(20, 300);
-    ctx.lineTo(20, 250);
-    ctx.lineTo(70, 250);
-    ctx.lineTo(70, 300);
-    ctx.moveTo(20, 250);
-    ctx.lineTo(45, 230);
-    ctx.lineTo(70, 250);
-    ctx.stroke();
-}
